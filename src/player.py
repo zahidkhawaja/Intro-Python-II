81984db88aea900182d6dafeb37ada468f2e63d8
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-09-02 15:43:08 -0400

    Initial player and room classes, import to adv.py

--- src/player.py
+++ src/player.py
@@ -1,2 +1,7 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, my_location):
+        self.name = name
+        self.my_location = my_location
